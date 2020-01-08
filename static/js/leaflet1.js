70382c62983a085ade43b3668422bfd90c852ae8
Maxwell Patterson <maxwellpatterson@maxwells-air.attlocal.net>
2020-01-08 14:21:19 -0600

    Made Legend on Leaflet1 better

--- static/js/leaflet1.js
+++ static/js/leaflet1.js
@@ -31,7 +31,7 @@ function genInfo() {
         elects.push(data.data[i].electricity_use_kbtu);
         // sizes.push(data.data[i].whateverthesizeis);
     }
-    console.log(ages);
+    // console.log(ages);
     // console.log(names);
     // console.log(lats);
     // Create tile layer
@@ -97,36 +97,36 @@ function genInfo() {
                 var energy = engs[j];
                 // var sqFt = sizes[j];
                 var age = ages[j];
-                console.log(age);
+                // console.log(age);
                 var color;
 
                 if (age < 1900) {
                     buildingAge = "age_less_1900";
-                    color = "#edf8fb";
+                    color = "red";
                 }
                 else if (age < 1920) {
                     buildingAge = "age_less_1920";
-                    color = "red";
+                    color = "orange-dark";
                 }
                 else if (age < 1940) {
                     buildingAge = "age_less_1940";
-                    color = "orange";
+                    color = "blue";
                 }
                 else if (age < 1960) {
                     buildingAge = "age_less_1960";
-                    color = "yellow";
+                    color = "cyan";
                 }
                 else if (age < 1980) {
                     buildingAge = "age_less_1980";
-                    color = "green";
+                    color = "violet";
                 }
                 else if (age < 2000) {
                     buildingAge = "age_less_2000";
-                    color = "blue";
+                    color = "green-dark";
                 }
                 else {
                     buildingAge = "age_less_2020";
-                    color = "purple";
+                    color = "black";
                 }
                 buildingCount[buildingAge]++;
                 // var latLong = [lat, long];
@@ -145,17 +145,17 @@ function genInfo() {
                     // .addTo(layers[buildingAge])
                 
                     .bindPopup("<h1>" + names[j] 
-                + "</h1> <hr> <h3>" 
+                + "</h1> <hr> <h5>" 
                 + "Energy Consumption: " 
                 + energy
-                + " kBtu/sq ft </h3> <h3>" 
+                + " kBtu/sq ft </h5> <h5>" 
                 // + "Square Footage: "
                 // + sqFt
-                // + " ft^2</h3>"
-                // + "<h3>"
+                // + " ft^2</h5>"
+                // + "<h5>"
                 + "Year Built: "
                 + age
-                + "</h3>"
+                + "</h5>"
                 ));
                 // locations.push(latLong);
                 // console.log(latLong);
@@ -171,14 +171,30 @@ function genInfo() {
     // Update the legend's innerHTML with the last updated time and station count
     function updateLegend(buildingCount) {
         document.querySelector(".legend").innerHTML = [
-        "<p class='before-1900'>Buildings Built Before 1900: " + buildingCount.age_less_1900 + "</p>",
-        "<p class='between-1900-1920'>Buildings Built Between 1900 and 1920: " + buildingCount.age_less_1920 + "</p>",
-        "<p class='between-1920-1940'>Buildings Built Between 1920 and 1940: " + buildingCount.age_less_1940 + "</p>",
-        "<p class='between-1940-1960'>Buildings Built Between 1940 and 1960: " + buildingCount.age_less_1960 + "</p>",
-        "<p class='between-1960-1980'>Buildings Built Between 1960 and 1980: " + buildingCount.age_less_1980 + "</p>",
-        "<p class='between-1980-2000'>Buildings Built Between 1980 and 2000: " + buildingCount.age_less_2000 + "</p>",
-        "<p class='between-2000-2020'>Buildings Built After 2000: " + buildingCount.age_less_2020 + "</p>"
-        ].join("");
+        // "<p class='before-1900'> <div class = 'block-1900' </div> Buildings Built Before 1900: " + buildingCount.age_less_1900 + " </p> <br/> <br/> <br/>",
+        // "<p class='between-1900-1920'>Buildings Built Between 1900 and 1920: " + buildingCount.age_less_1920 + "</p> ",
+        // "<p class='between-1920-1940'>Buildings Built Between 1920 and 1940: " + buildingCount.age_less_1940 + "</p> ",
+        // "<p class='between-1940-1960'>Buildings Built Between 1940 and 1960: " + buildingCount.age_less_1960 + "</p> ",
+        // "<p class='between-1960-1980'>Buildings Built Between 1960 and 1980: " + buildingCount.age_less_1980 + "</p> ",
+        // "<p class='between-1980-2000'>Buildings Built Between 1980 and 2000: " + buildingCount.age_less_2000 + "</p> ",
+        // "<p class='between-2000-2020'>Buildings Built After 2000: " + buildingCount.age_less_2020 + "</p>"
+        // ]
+       " <div class='my-legend'>" +
+            "<div class='legend-title'>Legend</div>" +
+            "<div class='legend-scale'>" +
+                "<ul class='legend-labels'>" +
+                    "<li><span style='background:#e3180e;'></span> Buildings Built Before 1900: " + buildingCount.age_less_1900 + "</li>" +
+                    "<li><span style='background:#e37a09;'></span>Buildings Built Between 1900 and 1920: " + buildingCount.age_less_1920 + "</li>" +
+                    "<li><span style='background:#0964e3;'></span>Buildings Built Between 1920 and 1940: " + buildingCount.age_less_1940 + "</li>" +
+                    "<li><span style='background:#09bfe3;'></span>Buildings Built Between 1940 and 1960: " + buildingCount.age_less_1960 + "</li>" +
+                    "<li><span style='background:#610d91;'></span>Buildings Built Between 1960 and 1980: " + buildingCount.age_less_1980 + "</li>" + 
+                    "<li><span style='background:#047836;'></span>Buildings Built Between 1980 and 2000: " + buildingCount.age_less_2000 + "</li>" + 
+                    "<li><span style='background:#000000;'></span>Buildings Built Between 2000 and 2020: " + buildingCount.age_less_2020 + "</li>" + 
+                "</ul>"+
+            "</div>"+
+            "<div class='legend-source'>Source: <a href='https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking/xq83-jr8c'>Chicago Energy Benchmarking</a></div>"+
+            "</div>"]
+        .join("");
     }
 })};
 
